1706cb8adf36d1d80a3b9122ad1c756aa9feda5b
RodakDev - VPS <rodak.titera@gmail.com>
2025-10-15 11:24:44 +0200

    remove: useless import

--- canvas-client/src/components/PaintCanvas.tsx
+++ canvas-client/src/components/PaintCanvas.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
 import type { Canvas } from "../models/canvas";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export interface PaintCanvasProps {
   canvas: Canvas;
